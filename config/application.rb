9fca4e896d309dfecc4fb2766a6ab8be153a4490
Tomoaki Suzuki <tomoaki.suzuki@pixta.co.jp>
2020-08-01 18:45:18 +0900

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
@@ -15,5 +16,6 @@ module SampleApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
